5d077b98fef62408cf0bc646e75addf6e1e82fbd
MergenEnBilge <abdullahkamran4@gmail.com>
2026-09-25 23:12:05 +0500

    Build a Debian package and publish the Docker image on release

---
+++ packaging/make-deb.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Wraps the PyInstaller output in dist/Katib into a .deb for Debian, Ubuntu and Raspberry Pi OS.
+#   packaging/make-deb.sh 0.1.0
+set -eu
+
+VERSION="${1:?Give the version, for example 0.1.0}"
+ARCH="$(dpkg --print-architecture)"
+ROOT="build/deb/katib_${VERSION}_${ARCH}"
+
+rm -rf "$ROOT"
+mkdir -p "$ROOT/DEBIAN" "$ROOT/opt/katib" "$ROOT/usr/bin" \
+  "$ROOT/usr/share/applications" "$ROOT/usr/share/icons/hicolor/256x256/apps"
+
+cp -r dist/Katib/. "$ROOT/opt/katib/"
+ln -s /opt/katib/Katib "$ROOT/usr/bin/katib-app"
+cp packaging/icon.png "$ROOT/usr/share/icons/hicolor/256x256/apps/katib.png"
+
+cat > "$ROOT/usr/share/applications/katib.desktop" <<DESKTOP
+[Desktop Entry]
+Type=Application
+Name=Katib
+Comment=Label images for machine learning
+Exec=/opt/katib/Katib
+Icon=katib
+Categories=Graphics;Development;
+Terminal=false
+DESKTOP
+
+cat > "$ROOT/DEBIAN/control" <<CONTROL
+Package: katib
+Version: $VERSION
+Section: graphics
+Priority: optional
+Architecture: $ARCH
+Maintainer: Katib maintainers <noreply@users.noreply.github.com>
+Depends: libgtk-3-0, gir1.2-webkit2-4.1 | gir1.2-webkit2-4.0
+Description: Image annotation tool
+ Katib lets you draw boxes, outlines, keypoints and text on pictures and
+ export them for training. It runs on your own computer.
+CONTROL
+
+dpkg-deb --build --root-owner-group "$ROOT" "dist/katib_${VERSION}_${ARCH}.deb"
